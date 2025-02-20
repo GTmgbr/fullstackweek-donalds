4a5f72e6f36ce8ac2291b21b7a8c85dc728d8f8d
GTmgbr <gustavo.totti14@gmail.com>
2025-02-20 02:21:11 -0300

    feat add unique constraint to restaurant slug

---
+++ src/app/[slug]/page.tsx
@@ -0,0 +1,67 @@
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
+import { db } from "@/lib/prisma";
+
+import ConsumptionMethodOption from "./components/consumption-method-option";
+
+interface RestaurantPageProps {
+  params: Promise<{ slug: string }>;
+}
+
+// server component
+const RestaurantPage = async ({ params }: RestaurantPageProps) => {
+  const { slug } = await params;
+  const restaurant = await db.restaurant.findFirst({ where: { slug } });
+  //const restaurant = await db.restaurant.findUnique({ where: { slug } });
+  if (!restaurant) {
+    return notFound();
+  }
+  return (
+    <div className="flex h-screen flex-col items-center justify-center px-6 pt-24">
+      {/* LOGO E TITULO */}
+      <div className="flex flex-col items-center gap-2">
+        <Image
+          src={restaurant.avatarImageUrl}
+          alt={restaurant.name}
+          width={82}
+          height={82}
+        />
+        <h2 className="font-semibold">{restaurant.name}</h2>
+      </div>
+      {/* BEM VINDO */}
+      <div className="space-y-2 pt-24 text-center">
+        <h3 className="text-2xl font-semibold">Seja bem-vindo!</h3>
+        <p className="opacity-55">
+          Escolha como prefere aproveitar sua refeição. Estamos oferecendo
+          praticidade e sabor em cada detalhe!
+        </p>
+      </div>
+      <div className="grid-cols-2 gap-4 pt-14">
+        <ConsumptionMethodOption
+          option="DINE_IN"
+          slug={slug}
+          buttonText="Para comer aqui"
+          imageAlt="Comer aqui"
+          imageUrl="/dine_in.png"
+        />
+        <ConsumptionMethodOption
+          slug={slug}
+          option="TAKEWAY"
+          buttonText="Para levar"
+          imageAlt="Para levar"
+          imageUrl="/takeaway.png"
+        />
+      </div>
+    </div>
+  );
+};
+
+// server components = renderizados no servidor
+// podem ser async
+// podem chamar recursos do back-end (banco de dados)
+// não pode usar hooks
+// não pode ter interatividade (se quiser ter, usar client component)
+// client component não pode ser async
+
+export default RestaurantPage;
